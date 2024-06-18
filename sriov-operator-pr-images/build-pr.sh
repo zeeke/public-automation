30e5c68c793583d013b05e5e2943977003c9d476
Andrea Panattoni <apanatto@redhat.com>
2024-06-18 16:10:38 +0200

    auto build

--- sriov-operator-pr-images/build-pr.sh
+++ sriov-operator-pr-images/build-pr.sh
@@ -15,3 +15,21 @@ cd /tmp/sriov-network-operator
 git checkout .
 
 gh pr checkout $url
+
+echo "ENV CLUSTER_TYPE=openshift" >> Dockerfile
+
+TAG=`git branch --show-current`-`date +%Y%m%d`-`git rev-parse --short HEAD`
+
+SRIOV_NETWORK_OPERATOR_IMAGE="ghcr.io/zeeke/sriov-network-operator:$TAG"
+SRIOV_NETWORK_CONFIG_DAEMON_IMAGE="ghcr.io/zeeke/sriov-network-operator-config-daemon:$TAG"
+SRIOV_NETWORK_WEBHOOK_IMAGE="ghcr.io/zeeke/sriov-network-operator-webhook:$TAG"
+
+docker build -t ${SRIOV_NETWORK_OPERATOR_IMAGE} -f "Dockerfile" .
+docker build -t ${SRIOV_NETWORK_CONFIG_DAEMON_IMAGE} -f Dockerfile.sriov-network-config-daemon .
+docekr build -t ${SRIOV_NETWORK_WEBHOOK_IMAGE} -f Dockerfile.webhook .
+
+echo $GITHUB_TOKEN | docker login ghcr.io -u zeeke --password-stdin
+
+docker push ${SRIOV_NETWORK_OPERATOR_IMAGE}
+docker push ${SRIOV_NETWORK_CONFIG_DAEMON_IMAGE}
+docker push ${SRIOV_NETWORK_WEBHOOK_IMAGE}
